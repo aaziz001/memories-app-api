c3b1666015d9d787bb62ae50ac38f4f3ce529e3c
Abdul Aziz Memon <aziz2001900@gmail.com>
2021-08-09 15:42:28 +0500

    first commit

---
+++ controllers/posts.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPosts = async (req, res) => {
+  const post = req.body;
+  const newPost = new PostMessage(post);
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
+
+export const updatePost = async (req, res) => {
+  const { id } = req.params;
+  const { title, creator, tags, message, selectedFile } = req.body;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("No post found with that id");
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    id,
+    { title, creator, message, tags, selectedFile, _id: id },
+    {
+      new: true,
+    }
+  );
+  res.json(updatedPost);
+};
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("No post found with that id");
+  await PostMessage.findByIdAndRemove(id);
+  console.log("DELETED");
+  res.json({ message: "Post Deleted Successfully" });
+};
+
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("No post found with that id");
+  const post = await PostMessage.findById(id);
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    id,
+    { likeCount: post.likeCount + 1 },
+    { new: true }
+  );
+  res.json(updatedPost);
+};
---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import postRoutes from "./routes/posts.js";
+
+dotenv.config();
+const app = express();
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 3001;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server Started on PORT: ${PORT}`))
+  )
+  .catch((err) => console.log(err.message));
