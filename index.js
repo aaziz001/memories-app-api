c5a2d07a3edd2fafb42602ad858d7bbe5af6f376
Abdul Aziz Memon <aziz2001900@gmail.com>
2021-08-13 22:25:14 +0500

    Added User Authentication routes and User model

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
 
 dotenv.config();
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
+app.use("/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the memories app api");
