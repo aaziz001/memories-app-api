cc87cef93c50a42c5d5c03b664e7c78ecff14000
Abdul Aziz Memon <aziz2001900@gmail.com>
2021-10-27 00:53:16 +0500

    debugging

--- controllers/posts.js
+++ controllers/posts.js
@@ -5,7 +5,7 @@ export const getPosts = async (req, res) => {
   try {
     const { page } = req.query;
 
-    const LIMIT = 8;
+    const LIMIT = 4;
     const startIndex = (Number(page) - 1) * LIMIT;
     const total = await PostMessage.countDocuments({});
 
